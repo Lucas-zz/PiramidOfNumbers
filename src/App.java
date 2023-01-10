dcefab9bd2d23c6ac187a0a25e17d9a37a9f6d5a
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-01-10 17:57:19 -0300

    feat: input number from the user to create the piramid

--- src/App.java
+++ src/App.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int num = 9;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Insira um número: ");
+            int num = sc.nextInt();
 
-        for (int i = 1; i <= num; i++) {
-            String strFormatada = "";
-            for (int j = 1; j <= i; j++) {
-                strFormatada += i;
+            System.out.println("\n"); // New line
+
+            for (int i = 1; i <= num; i++) {
+                String strFormatada = "";
+                for (int j = 1; j <= i; j++) {
+                    strFormatada += i;
+                }
+
+                System.out.println(strFormatada);
             }
-
-            System.out.println(strFormatada);
         }
     }
 }
